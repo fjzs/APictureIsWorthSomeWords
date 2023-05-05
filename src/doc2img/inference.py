f61ff5f9a0ecbb24c4a93a9a611a73c9df92d4c8
Pushkar-Bhuse <YOUR_EMAIL>
2023-05-04 22:28:48 -0700

    Pretrained CLIP

--- src/doc2img/inference.py
+++ src/doc2img/inference.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 import argparse
-from transformers import DistilBertTokenizer
+from transformers import DistilBertTokenizer, CLIPProcessor, CLIPModel
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -12,6 +12,22 @@ import config as CFG
 from train import build_loaders
 from encoders.clip import CLIPModel
 import albumentations as A
+from PIL import Image
+
+
+def get_pretrained_clip_scores(image_path, text):
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+    image = Image.open(image_path)
+
+    inputs = processor(text=[text], images=image, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+    # probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+
+    return logits_per_image
+
 
 def get_image_embeddings(valid_df, model_path):
     tokenizer = DistilBertTokenizer.from_pretrained(CFG.text_tokenizer)
