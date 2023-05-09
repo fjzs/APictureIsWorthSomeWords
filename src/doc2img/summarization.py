3ca3fb19f8c1b48c06af0f9dfbcba394918ba3dc
prash29 <gillyprash29@gmail.com>
2023-05-08 17:14:15 -0700

    updated summarization.py

--- src/doc2img/summarization.py
+++ src/doc2img/summarization.py
@@ -2,16 +2,24 @@
 # Given a big input of text, the output is a shorter text
 
 from transformers import pipeline
-import configparser
-from utils.conf import *
 
-config = Conf('utils/conf.txt')
+def get_summary(config, df):
+    if config['default']['summary_method'] == 'tf-idf':
+        pass
+    elif config['default']['summary_method']=='hf':
+        return text_summarization_hf(config, df)
 
-def text_summarization_hf(text, config):
-    summarizer =  pipeline("summarization", model=config.summarization['model'],
-                            min_length=config.summarization['min_length'], max_length=config.summarization['max_length'])
-    # We can add some preprocessing for text here 
-    text = text.replace("\n", " ")
-    ans = summarizer(text)
-    return ans[0]['summary_text']
+
+def text_summarization_hf(config, df):
+    summarizer =  pipeline("summarization", model=config['summarization']['model'],
+                            min_length=config['summarization']['min_length'], max_length=config['summarization']['max_length'])
+    
+    def summarize(text, summarizer):
+        # We can add some preprocessing for text here 
+        text = text.replace("\n", ". ")
+        ans = summarizer(text)
+        return ans[0]['summary_text']
+
+    df['summarized_text'] = df['text'].apply(lambda x: summarize(x, summarizer))
+    return df
     
