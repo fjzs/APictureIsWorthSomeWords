7a1cb9c446729bc5fcb5ce8aadfa597f29b044c7
Prashant <gillyprash29@gmail.com>
2023-05-20 23:55:17 +0000

    Generated images and df

--- src/main.sh
+++ src/main.sh
@@ -3,6 +3,10 @@
 config_dir="doc2img/configs"
 for config in "$config_dir"/*
 do
+    if [[ "$config" == *"hf"* ]]; then
+        continue
+    fi
     echo "$config"
+    cat $config > config.yaml
     python main.py $config
 done
