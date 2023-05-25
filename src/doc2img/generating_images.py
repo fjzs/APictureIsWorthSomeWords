3a7c9ecda4d6c52694e2269a1351f9ae46841612
Pushkar-Bhuse <YOUR_EMAIL>
2023-05-25 00:10:55 -0700

    Provision for prompts

--- src/doc2img/generating_images.py
+++ src/doc2img/generating_images.py
@@ -4,6 +4,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 import yaml
 import os
+import pandas as pd
 
 #setting device to gpu if available
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -11,7 +12,7 @@ import os
 #reading config file
 config_file = './config.yaml'
 with open(config_file) as cf_file:
-    config = yaml.safe_load( cf_file.read())
+    config = yaml.safe_load(cf_file.read())
 
 #model and seed
 model_id = config['image_generation']['model_id']
@@ -19,7 +20,8 @@ seed = config['image_generation']['seed']
 inference_steps = config['image_generation']['inference_steps']
 
 #save parameters
-save_folder = config['image_generation']['save_folder']
+save_folder = "experiment_tfidf_poems_new"
+# config['image_generation']['save_folder']
 save_flag = config['image_generation']['save_flag']
 
 #creating model
@@ -28,29 +30,41 @@ pipe = pipe.to(config['device'])
 
 
 
-def generate_image(df):
+def generate_image(df, prompts):
     
     #Inputs:
     #  df: dataframe contains summaries
+    #  prompts: list of required prompts
     #Outputs:
     #  df: datarframe containing summaries and associated saved image paths
     
     
     os.mkdir(save_folder)
-    save_paths = []
+    save_paths = {
+        "prompt_"+str(idx): [] for idx in range(len(prompts))
+    }
     
     #Generating images one by one
-    for index,prompt in enumerate(df['summary']):
+    for index,text in enumerate(df['summary']):
+
+        for prompt_idx, prompt in enumerate(prompts):
+
+            text = prompt + text
         
-        #setting seed
-        generator = torch.Generator(config['device']).manual_seed(seed)
-        
-        image = pipe(prompt, generator=generator, num_inference_steps=inference_steps, output_type="np").images
-        
-        #saving images 
-        if save_flag:
-            save_path = os.path.join(save_folder,str(index) +'.jpg')
-            save_paths.append(save_path)
-            plt.imsave(save_path, image[0])
-    df['img_path'] = save_paths
+            #setting seed
+            generator = torch.Generator(config['device']).manual_seed(seed)
+            
+            image = pipe(text, generator=generator, num_inference_steps=inference_steps, output_type="np").images
+
+            # image = np.zeros([1,100,100,3],dtype=np.uint8)
+            # image.fill(255)
+            
+            #saving images 
+            if save_flag:
+                final_index = config['datasets']['type'] + "_" + str(index) + "_prompt_" + str(prompt_idx)
+                save_path = os.path.join(save_folder,str(final_index) +'.jpg')
+                save_paths["prompt_"+str(prompt_idx)].append(save_path)
+                plt.imsave(save_path, image[0])
+
+    df = pd.concat([df, pd.DataFrame.from_dict(save_paths)], axis=1)
     return df
