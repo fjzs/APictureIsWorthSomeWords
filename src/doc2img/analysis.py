bd879c9b3c148a1261bc9c7ad525fb14c4610e32
Pushkar-Bhuse <YOUR_EMAIL>
2023-05-30 22:31:10 -0700

    Updated CLIP analysis to median

--- src/doc2img/analysis.py
+++ src/doc2img/analysis.py
@@ -6,7 +6,7 @@ import json
 # which prompt gives the best CLIP score
 
 # Second Analysis: For a given method on a particular dataset,
-# what is the average Max CLIP score from the 
+# what is the average Median CLIP score from the 
 # best performing prompts on each image.
 def quant_analysis(df, prompt_list):
     best_prompts = []
@@ -18,11 +18,11 @@ def quant_analysis(df, prompt_list):
         best_prompt = None
         curr_best = float("-inf")
         for prompt_score, prompt in prompt_dict.items():
-            # Currently only doing this analysis on MAX CLIP Score 
+            # Currently only doing this analysis on Median CLIP Score 
             # Which is the first score in the tuple.
             scores = json.loads(row[prompt_score])
-            if scores[0] > curr_best:
-                curr_best = scores[0]
+            if scores[2] > curr_best:
+                curr_best = scores[2]
                 best_prompt = prompt
         best_prompts.append(best_prompt)
         best_scores.append(curr_best)
@@ -31,9 +31,9 @@ def quant_analysis(df, prompt_list):
     df['best_prompt'] = best_prompts
     df['best_prompt'].value_counts().plot.barh()
 
-    # Getting Average CLIP Score
-    df['best_clip_max'] = best_scores
-    print("Average Max CLIP Score: {}".format(np.average(best_scores)))
+    # Getting Average Median CLIP Score
+    df['best_clip_median'] = best_scores
+    print("Average Median CLIP Score: {}".format(np.average(best_scores)))
 
     return df
 
